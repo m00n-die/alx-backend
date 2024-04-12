b838c674827ed87391044570360dc6196c27ea13
m00n-die <reginaldrock219@gmail.com>
2024-04-12 16:00:03 +0100

    bug fixes

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,11 +1,12 @@
-#!/usr.bin/env python3
+#!/usr/bin/env python3
 """A simple flask application"""
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_babel import Babel
 
 
 class Config():
+    """Basic config class for babel"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
@@ -14,6 +15,11 @@ class Config():
 app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
+
+@app.route('/')
+def hello_world():
+    """returns index.html file"""
+    return render_template('1-index.html')
 
 if __name__ == '__main__':
     app.run()
