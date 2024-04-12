22d3d04e7429e9a5bcb5f8976a78962f9c7c142f
m00n-die <reginaldrock219@gmail.com>
2024-04-12 15:29:38 +0100

    pycodestyle validation

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,10 +1,16 @@
+#!/usr/bin/env python3
+"""A simple flask application"""
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
+    """function to render a template"""
     return render_template("0-index.html")
+
 
 if __name__ == '__main__':
     app.run()
