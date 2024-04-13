f3ee590502b215d86df2aa1eef85a6efdb8ad850
m00n-die <reginaldrock219@gmail.com>
2024-04-13 22:11:18 +0100

    task 03

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""A simple flask application"""
+
+from flask import Flask, render_template, g, request, gettext
+from flask_babel import Babel
+
+
+class Config():
+    """Basic config class for babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """determine best language match"""
+    user = getattr(g, 'user', None)
+    if user is not None:
+        return user.locale
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+@app.route('/')
+def hello_world():
+    """returns index.html file"""
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
