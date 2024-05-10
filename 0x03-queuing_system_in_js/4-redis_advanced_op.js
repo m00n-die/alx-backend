fbdf861aad7dfef5267c6c4c983575a6e1507ffe
m00n-die <reginaldrock219@gmail.com>
2024-05-10 05:35:04 +0100

    task 04 done

--- 0x03-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -2,27 +2,28 @@ import redis from "redis";
 
 const client = redis.createClient();
 
-client.on("error", (error) => {
-  if (error) console.log(`Redis client not connected to the server: ${err}`);
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
 });
+
+client.on('error', (error) => {
+  console.log(`Redis client not connected to the server: ${error}`);
+});
+
+
+client.hset('HolbertonSchools', 'Portland', '50', print);
+client.hset('HolbertonSchools', 'Seattle', '80', print);
+client.hset('HolbertonSchools', 'New York', '20', print);
+client.hset('HolbertonSchools', 'Bogota', '20', print);
+client.hset('HolbertonSchools', 'Cali', '40', print);
+client.hset('HolbertonSchools', 'Paris', '2', print);
+
+
+client.hgetall('HolbertonSchools', (error, result) => {
+  if (error) {
+    console.log(error);
+    throw error;
+  }
   
-client.on("ready", () => {
-    console.log("Redis client connected to the server");
+  console.log(result);
 });
-
-const sch_name = "HolbertonSchools";
-
-const values = {
-  Portland: 50,
-  Seattle: 80,
-  "New York": 20,
-  Bogota: 20,
-  Cali: 40,
-  Paris: 2,
-};
-
-for (const [key, val] of Object.entries(values)) {
-  client.hset(sch_name, key, val, (_, reply) => redis.print(`Reply: ${reply}`));
-}
-
-client.hgetall(sch_name, (_, object) => console.log(object));
